11877c53cafd11a006088789668d73c8104ba4c4
Don Peterkofsky <donpeterkofsky@MacBook-Pro.local>
2017-01-05 11:24:48 -0500

    Community validation.

---
+++ src/model/communityResource.ts
@@ -0,0 +1,45 @@
+import {computedFrom} from 'aurelia-framework';
+
+/**
+ *  Generic controller for model-mutating dialogs. 
+ * */
+export class CommunityResource {
+  _name: string;
+  _description: string;
+  communityType: string;
+  communityId: string;
+  
+  constructor(community?:any) {
+      if(community && community !== null) {
+        this._name = community.communityName;
+        this._description = community.communityDescription;
+        this.communityType = community.communityType;
+        this.communityId = community.communityId;
+      } else {
+        this._name = '';
+        this._description = '';
+        this.communityType = '';
+      }
+  }
+
+  @computedFrom('_name')
+  get communityName() {
+    return typeof this._name === 'string'?this._name.trim():'';
+  }
+  set communityName(value) {
+    this._name = typeof value === 'string'?value.trim():'';
+  }
+
+  @computedFrom('_description')
+  get communityDescription() {
+    return typeof this._description === 'string'?this._description.trim():'';
+  }
+  set communityDescription(value) {
+    this._description = typeof value === 'string'?value.trim():'';
+  }
+
+}
+
+
+
+
