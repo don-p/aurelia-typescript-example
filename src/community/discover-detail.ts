bee03463fdae3c0e17169dd795e500c50aaec232
Don Peterkofsky <donpeterkofsky@MacBook-Pro.local>
2017-02-13 16:31:37 -0500

    Refactor member action buttons as component.  Switch detail view dynamically based on router settings.

--- src/community/discover-detail.ts
+++ src/community/discover-detail.ts
@@ -19,7 +19,7 @@ import {Utils} from '../services/util';
 
 @inject(Session, Router, DataService, CommunityService, OrganizationService, EventAggregator, 
   Ps, I18N, AureliaConfiguration, Utils, Parent.of(Community), LogManager) // SCROLL
-export class CommunityDetail {
+export class DiscoverDetail {
   member: Object;
 
   navigationInstruction: NavigationInstruction;
@@ -98,90 +98,6 @@ export class CommunityDetail {
     this.logger = LogManager.getLogger(this.constructor.name);
   }
 
-  // getGridColumns(type: string) { 
-  //   let columns = [];
-  //   // return [
-  //   columns.push({
-  //     colId: 'select',
-  //     headerName: '', 
-  //     width: 30, 
-  //     minWidth: 30, 
-  //     checkboxSelection: true, 
-  //     suppressMenu: true
-  //   });
-  //   columns.push({
-  //     headerName: this.i18n.tr('community.members.firstname'), 
-  //     field: "physicalPersonProfile.firstName",
-  //     filter: TextSearchFilter
-  //   });
-  //   columns.push({
-  //     headerName: this.i18n.tr('community.members.lastname'), 
-  //     field: "physicalPersonProfile.lastName", 
-  //     filter: TextSearchFilter
-  //   });
-  //   if(type == 'listMembers') {
-  //     columns.push({
-  //       headerName: this.i18n.tr('community.members.organization'), 
-  //       field: "physicalPersonProfile.organization.organizationName",
-  //       filter: TextSearchFilter,
-  //       hide: false
-  //     });
-  //   } // else if (type === 'addMembers') {
-  //     columns.push({
-  //       headerName: this.i18n.tr('community.members.title'), 
-  //       field: "physicalPersonProfile.jobTitle",
-  //       filter: TextSearchFilter
-  //     });
-  //   // }
-  //   columns.push({
-  //     headerName: this.i18n.tr('community.members.city'), 
-  //     field: "physicalPersonProfile.locationProfile.city",
-  //     filter: TextSearchFilter
-  //   });
-  //   columns.push({
-  //     headerName: this.i18n.tr('community.members.state'), 
-  //     field: "physicalPersonProfile.locationProfile.stateCode", 
-  //     filter: TextSearchFilter,
-  //     width: 100
-  //   });
-  //   columns.push({
-  //     headerName: this.i18n.tr('community.members.zip'), 
-  //     field: "physicalPersonProfile.locationProfile.zipCode", 
-  //     filter: TextSearchFilter,
-  //     width: 80
-  //   });
-
-  //   return columns;
-  // }
-
-  // getGridOptions(type): GridOptions {
-  //   let me = this;
-  //     return {
-  //     columnDefs: this.getGridColumns(type),
-  //     rowSelection: 'multiple',
-  //     rowHeight: 30,
-  //     headerHeight: 40,
-  //     suppressMenuHide: true,
-  //     // pageSize: this.pageSize,
-  //     paginationPageSize: this.pageSize,
-  //     sortingOrder: ['desc','asc'],
-  //     enableServerSideSorting: true,
-  //     enableServerSideFilter: true,
-  //     enableColResize: true,
-  //     debug: false,
-  //     rowModelType: 'virtual',
-  //     maxPagesInCache: 2,
-  //     onViewportChanged: function() {
-  //       if(!this.api) return;
-  //       this.api.sizeColumnsToFit();
-  //     },
-  //     onGridSizeChanged: function(){
-  //       if(!this.api) return;
-  //       this.api.sizeColumnsToFit();
-  //     }
-  //   };
-  // }
-
   activate(params, navigationInstruction) {
   }
 
@@ -237,99 +153,6 @@ export class CommunityDetail {
     // this.agGridWrap.gridCreated = true;
     this.gridOptions['api'].sizeColumnsToFit();
   }
-
-  // setCommunityMembersGridDataSource(dataSourceName, gridOptions, pageSize, communityService, selection, showSelected) {
-  //   const me = this;
-
-  //   // Adjust column visibility based on community type - TEAM or COI.
-  //   let type = this.selectedCmty.communityType;
-  // //   if(type === 'TEAM') {
-  // //     // Show title and org column.
-  // //     gridOptions.columnApi.setColumnVisible('physicalPersonProfile.organization.organizationName', true);
-  // //     gridOptions.columnApi.setColumnVisible('physicalPersonProfile.jobTitle', true);      
-  // //     gridOptions.api.sizeColumnsToFit();
-  // //   } else {
-  // //     gridOptions.columnApi.setColumnVisible('physicalPersonProfile.organization.organizationName', true);      
-  // //     gridOptions.api.sizeColumnsToFit();
-  // //     gridOptions.columnApi.autoSizeColumn('physicalPersonProfile.organization.organizationName');
-  // //  }
-  //   let name = dataSourceName; //showSelected?'selectedCommunityMembers':'communityMembers';
-  //   // let selectionFilterComponent:SelectedRowFilter = gridOptions.api.getFilterInstance('select');
-  //   // if(showSelected) {
-  //   //   selectionFilterComponent.setActive(true);
-  //   //   // gridOptions.columnDefs[0].filter = new SelectedRowFilter();
-  //   // } else {
-  //   //   selectionFilterComponent.setActive(false);
-  //   //   // gridOptions.columnDefs[0].filter = null;
-  //   // }
-
-  //   selection = gridOptions.selection;
-
-  //   let gridDataSource = {
-  //       /** If you know up front how many rows are in the dataset, set it here. Otherwise leave blank.*/
-  //       name: name,
-  //       rowCount: null,
-  //       paginationPageSize: pageSize,
-  //       //  paginationOverflowSize: 1,
-  //         maxConcurrentDatasourceRequests: 2,
-  //       //  maxPagesInPaginationCache: 2,
-  //       loading: false,
-
-  //       /** Callback the grid calls that you implement to fetch rows from the server. See below for params.*/
-  //       getRows: function(params: IGetRowsParams) {
-  //         gridOptions.api.showLoadingOverlay();
-  //         let selection = gridOptions.selection;
-  //         // if(!this.loading) {
-  //           me.logger.debug("..... setCommunityMembersGridDataSource Loading Grid rows | startIndex: " + params.startRow);
-  //           me.logger.debug("..... ..... Filter | " + Object.keys(params.filterModel));
-  //           me.logger.debug("..... ..... Sort | " + params.sortModel.toString());
-  //           this.loading = true;
-  //           let filter = me.findGridColumnDef(Object.keys(params.filterModel)[0]);
-  //           me.logger.debug('Filter >> :' + JSON.stringify(params.filterModel));
-  //           let  communityId = gridOptions.communityId;
-  //           let membersPromise = communityService.getCommunity(communityId, params.startRow, pageSize, params);
-  //           membersPromise.then(response => response.json())
-  //             .then(data => {
-  //               // if(gridDataSource.rowCount === null) {
-  //                 gridDataSource.rowCount = data.totalCount;
-  //               // }
-  //               /*
-  //               // Filter out only selectedItems.
-  //               if(showSelected) {
-  //                 let filteredData = [];
-  //                 let rows:Array<any> = data.responseCollection;
-  //                 rows.forEach(function(node, index, array) {
-  //                   if (selection.find(function(item:any, index:number, array:any[]) {
-  //                     return item.memberId === node.memberId;
-  //                   })) {
-  //                       filteredData.push(node);
-  //                   }
-  //                 });
-  //                 data.responseCollection = filteredData;
-  //                 data.totalCount = filteredData.length;
-  //               }
-  //               */
-  //               params.successCallback(data.responseCollection, data.totalCount);
-  //               // pre-select nodes as needed.
-  //               if(Array.isArray(selection)) {
-  //                 gridOptions.api.forEachNode( function (node) {
-  //                     if (selection.find(function(item:any, index:number, array:any[]) {
-  //                       return item.memberId === node.data.memberId
-  //                     })) {
-  //                         node.setSelected(true);
-  //                         gridOptions.api.refreshRows([node]);
-  //                     }
-  //                 });
-  //               }
-  //               gridOptions.api.hideOverlay();
-                
-  //               this.loading = false;
-  //           });
-  //         // }
-  //       }
-  //   }
-  //   gridOptions.api.setDatasource(gridDataSource);
-  // }
 
   setSelectedCommunityMembersGridDataSource(dataSourceName, gridOptions, pageSize, communityService, selection, showSelected) {
     const me = this;
@@ -731,6 +554,80 @@ export class CommunityDetail {
     
   }
 
+  makeCallCommunityMembers() {
+    let maxParticipants = this.appConfig.get('server.MAX_CONFERENCE_PARTICIPANTS');
+    this.logger.debug('makeCallCommunityMembers() => MAX_CONFERENCE_PARTICIPANTS = ' + maxParticipants);
+
+    let message = null;
+    var me = this;
+    let communityMembers:any[];
+    communityMembers = this.gridOptions.api.getSelectedRows();
+
+    if(communityMembers.length === 1) {
+      message = this.i18n.tr('community.communities.members.call.callConfirmMessageSingle', 
+          {memberName: communityMembers[0].physicalPersonProfile.firstName + ' ' +
+          communityMembers[0].physicalPersonProfile.lastName});
+    } else if(communityMembers.length >= 1) {
+      message = this.i18n.tr('community.communities.members.call.callConfirmMessage',
+          {memberCount: communityMembers.length});
+    }
+    const vRules = ValidationRules
+      .ensure('item').maxItems(maxParticipants)
+      .withMessage(this.i18n.tr('community.communities.call.callParticipantMaxCountError', {count:maxParticipants}))
+      .rules;
+
+    this.dataService.openPromptDialog(this.i18n.tr('community.communities.members.call.title'),
+      message,
+      communityMembers, this.i18n.tr('button.call'), true, vRules, 'modelPromise', '')
+    .then((controller:any) => {
+      let model = controller.settings;
+      // Callback function for submitting the dialog.
+      controller.viewModel.submit = (communityMembers:any[]) => {
+        // Add logged-in user to the call list.
+        communityMembers.unshift(me.session.auth['member']);
+        let memberIDs = communityMembers.map(function(value) {
+          return {
+            "participantId": value.memberId,
+            "participantType": "MEMBER"
+          }
+        });
+        // Call the service to start the call.
+        let modelPromise = this.communityService.startConferenceCall({participantRef:memberIDs});
+        controller.viewModel.modelPromise = modelPromise;        
+        modelPromise
+        .then(response => response.json())
+        .then(data => {
+            // Update the message for success.
+            controller.viewModel.messagePrefix = 'global.success';
+            controller.viewModel.status = 'OK';
+            controller.viewModel.message = this.i18n.tr('community.communities.members.call.callSuccessMessage');
+            controller.viewModel.okText = this.i18n.tr('button.ok');
+            controller.viewModel.showCancel = false;
+            // Close dialog on success.
+            delete controller.viewModel.submit;
+          }, error => {
+            controller.viewModel.messagePrefix = 'global.failed';
+            controller.viewModel.status = 'ERROR';
+            model.errorMessage = this.i18n.tr('community.communities.members.call.callFailedMessage'); 
+            me.logger.error("Community member call() rejected."); 
+          }).catch(error => {
+            controller.viewModel.messagePrefix = 'global.failed';
+            controller.viewModel.status = 'ERROR';
+            model.errorMessage = this.i18n.tr('community.communities.members.call.callFailedMessage'); 
+            me.logger.error("Community member call() failed."); 
+            me.logger.error(error); 
+            return Promise.reject(error);
+          })
+      };
+      controller.result.then((response) => {
+        if (response.wasCancelled) {
+          // Cancel.
+          this.logger.debug('Cancel');
+        }
+      })
+    });
+  }
+
   sendConnectionRequest() {
 
     let message = null;
@@ -802,43 +699,6 @@ export class CommunityDetail {
       })
     });
   }
-  
-/*
-  loadData() {
-    //tell grid to set loading overlay while we get our data
-    this.membersGrid['ctx'].setLoadingOverlay(true);
-    
-    //set limit//offset in our dataclass
-    this.remoteData.setLimit(40);
-    this.remoteData.setOffset(0);
-    
-    //get the data from class
-    this.remoteData.getData()
-      .then((data)=>{
-        //set data to grid (the data have limit % length included)
-        //-> data ={col:data.result, length:data.length, limit:40}
-        // you could include offset here if you wanted..
-        this.membersGrid['ctx'].setData(data);
-      })
-  }
-
-  callRemoteServer(param){//filterArray, orderByArray, callback) {
-
-    this.remoteData.createOrderByString(param.sort);
-    this.remoteData.createQueryString(param.filter);
-    this.remoteData.setLimit(param.limit);
-    this.remoteData.setOffset(param.offset);
-
-    return this.remoteData.getData()
-      .then((data)=> {
-        return data;
-      }).catch((err)=> {
-        console.error(err);
-        //param.callback([]);
-      });
-  }
-*/
-
 
 }
 
