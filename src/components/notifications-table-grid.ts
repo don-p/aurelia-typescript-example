a80d73117490789c354e34e114a97620c5f47523
belp-blg <belp_on@yahoo.com>
2017-04-26 11:19:43 -0400

    CSS fix for alerts badges class names

--- src/components/notifications-table-grid.ts
+++ src/components/notifications-table-grid.ts
@@ -124,7 +124,7 @@ export class NotificationsTableGridCustomElement {
         var template = //'<div></div>';
             '<div class="full-width-panel full-width-notification-message">' +
             '  <div class="full-width-summary">' +
-            '    <label class="alert-label ' + data.notificationCategory.categoryName.substring(0,4) + '">' + data.notificationCategory.categoryName.split(" ")[0] + '</label><span>'+data.message+'</span>'+
+            '    <label class="alert-label ' + data.notificationCategory.categoryName.split(" ")[0] + '">' + data.notificationCategory.categoryName.split(" ")[0] + '</label><span>'+data.message+'</span>'+
             '  </div>' +
             '</div>';
 
